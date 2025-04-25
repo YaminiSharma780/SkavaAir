ad9a7611cc7bdf77dbc39c0f26545ba6573a8242
Yamini <sharmayamini1999@gmail.com>
2025-04-26 02:16:37 +0530

    changed App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div>
         <h1>Welcome to SkavaAir : always stay ahead of your deadline..</h1>
-        <h3>omg..</h3>
+        <h3>I am good to travel..</h3>
       </div>
     </>
   );
