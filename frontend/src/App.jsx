832ef53171625de1916a7467bef7ca1defd4ca42
Yamini <sharmayamini1999@gmail.com>
2025-04-25 23:26:41 +0530

    retriggering deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div>
         <h1>Welcome to SkavaAir : always stay ahead of your deadline..</h1>
-        <h3>Let's travel the whole world..</h3>
+        <h3>Let's gooooo..</h3>
       </div>
     </>
   );
