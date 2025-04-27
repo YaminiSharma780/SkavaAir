e8f80eb3bc7f3bed404ff0b0c81376c16c1eb780
Yamini <sharmayamini1999@gmail.com>
2025-04-27 15:46:55 +0530

    testing after adding code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <div>
         <h1>Welcome to SkavaAir : always stay ahead of your deadline ...</h1>
+        <h3>Launching soon ✈.....</h3>
       </div>
     </>
   );
